d88794522ff10af10ef3b7c2994c49612b48e19a
Padli <p0380054@gmail.com>
2026-06-02 01:42:49 +0700

    Add joke routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\JokeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,3 +18,7 @@ Route::get('/experience', [PortfolioController::class, 'experience'])->name('exp
 Route::get('/contact', [PortfolioController::class, 'contact'])->name('contact');
 Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
 
+// Joke Generator Routes
+Route::get('/joke', [JokeController::class, 'showJokePage'])->name('joke.page');
+Route::get('/api/joke/random', [JokeController::class, 'getRandomJoke'])->name('joke.random');
+Route::get('/api/joke/type/{type}', [JokeController::class, 'getJokeByType'])->name('joke.by-type');
